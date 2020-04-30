00f7b9e629ce4d0be2da0aea94cb46311e785f2c
diomfz <64569909+diomfz@users.noreply.github.com>
2020-04-30 15:03:30 +0700

    week2
    ganti warna, profile,

--- app/src/main/java/com/example/galaw/actEditProfile.java
+++ app/src/main/java/com/example/galaw/actEditProfile.java
@@ -2,13 +2,31 @@ package com.example.galaw;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class actEditProfile extends AppCompatActivity {
+
+    Button save;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_act_edit_profile);
+
+        save = findViewById(R.id.save);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent( actEditProfile.this, fragProfile.class);
+                startActivity(intent);
+
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/galaw/actHome.java
+++ app/src/main/java/com/example/galaw/actHome.java
@@ -4,39 +4,53 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class actHome extends AppCompatActivity  {
 
-    Button homeButton;
-    Button publicButton;
-    Button healButton;
-    Button profileButton;
 
-
+    ImageButton setting;
+    Button[] navigationButton = new Button[4];
+    int indexNav;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_act_home);
 
-        homeButton = findViewById(R.id.homeButton);
-        publicButton = findViewById(R.id.publicButton);
-        healButton = findViewById(R.id.healButton);
-        profileButton = findViewById(R.id.profileButton);
+        indexNav = 0;
+        navigationButton[0] = findViewById(R.id.homeButton);
+        navigationButton[1] = findViewById(R.id.publicButton);
+        navigationButton[2] = findViewById(R.id.healButton);
+        navigationButton[3] = findViewById(R.id.profileButton);
+        setting = findViewById(R.id.setting);
 
-        homeButton.setOnClickListener(new View.OnClickListener() {
+        setting.setVisibility(View.GONE);
+
+        navigationButton[0].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
                 ft.replace(R.id.frameLayout, new fragHome());
                 ft.commit();
+
+                navigationButton[indexNav].setTextColor((Color.parseColor("#000000")));
+                navigationButton[indexNav].setBackgroundColor((Color.parseColor("#FFFFFF")));
+
+                setting.setVisibility(View.GONE);
+
+                indexNav = 0;
+
+                navigationButton[indexNav].setTextColor((Color.parseColor("#ffffff")));
+                navigationButton[indexNav].setBackgroundColor((Color.parseColor("#7B5CFF")));
             }
         });
 
-        publicButton.setOnClickListener(new View.OnClickListener() {
+        navigationButton[1].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
@@ -44,10 +58,18 @@ public class actHome extends AppCompatActivity  {
                 ft.replace(R.id.frameLayout, new fragPublic());
                 ft.commit();
 
+                navigationButton[indexNav].setTextColor((Color.parseColor("#000000")));
+                navigationButton[indexNav].setBackgroundColor((Color.parseColor("#FFFFFF")));
+
+                setting.setVisibility(View.GONE);
+                indexNav = 1;
+
+                navigationButton[indexNav].setTextColor((Color.parseColor("#ffffff")));
+                navigationButton[indexNav].setBackgroundColor((Color.parseColor("#7B5CFF")));
             }
         });
 
-        healButton.setOnClickListener(new View.OnClickListener() {
+        navigationButton[2].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
@@ -55,10 +77,18 @@ public class actHome extends AppCompatActivity  {
                 ft.replace(R.id.frameLayout, new fragHeal());
                 ft.commit();
 
+                navigationButton[indexNav].setTextColor((Color.parseColor("#000000")));
+                navigationButton[indexNav].setBackgroundColor((Color.parseColor("#FFFFFF")));
+
+                setting.setVisibility(View.GONE);
+                indexNav = 2;
+
+                navigationButton[indexNav].setTextColor((Color.parseColor("#ffffff")));
+                navigationButton[indexNav].setBackgroundColor((Color.parseColor("#7B5CFF")));
             }
         });
 
-        profileButton.setOnClickListener(new View.OnClickListener() {
+        navigationButton[3].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
@@ -66,8 +96,25 @@ public class actHome extends AppCompatActivity  {
                 ft.replace(R.id.frameLayout, new fragProfile());
                 ft.commit();
 
+                navigationButton[indexNav].setTextColor((Color.parseColor("#000000")));
+                navigationButton[indexNav].setBackgroundColor((Color.parseColor("#FFFFFF")));
+
+                setting.setVisibility(View.VISIBLE);
+                indexNav = 3;
+
+                navigationButton[indexNav].setTextColor((Color.parseColor("#ffffff")));
+                navigationButton[indexNav].setBackgroundColor((Color.parseColor("#7B5CFF")));
             }
         });
+
+        setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(actHome.this, actEditProfile.class);
+            startActivity(intent);
+            }
+        });
+
 
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.replace(R.id.frameLayout, new fragHome());
