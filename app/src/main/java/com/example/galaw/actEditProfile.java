a4f7b0776aca040365e7c63bcf61c34341fcdd8f
repkeeh <64399425+repkeeh@users.noreply.github.com>
2020-05-05 15:15:01 +0700

    Activity Profile
    Yang belom :
     - Set Profile Picture
    - Verify Email

--- app/src/main/java/com/example/galaw/actEditProfile.java
+++ app/src/main/java/com/example/galaw/actEditProfile.java
@@ -1,14 +1,107 @@
 package com.example.galaw;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import javax.annotation.Nullable;
 
 public class actEditProfile extends AppCompatActivity {
+    TextView name,email,phone;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userID;
+    TextView resetPass;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_act_edit_profile);
+        name = findViewById(R.id.nameProfile);
+        email = findViewById(R.id.emailProfile);
+        phone = findViewById(R.id.phoneProfile);
+        resetPass = findViewById(R.id.resetPassword );
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+        userID = fAuth.getCurrentUser().getUid();
+        user = fAuth.getCurrentUser();
+
+        DocumentReference documentReference = fStore.collection("Users").document(userID);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                name.setText(documentSnapshot.getString("Name"));
+                email.setText(documentSnapshot.getString("Email"));
+                phone.setText(documentSnapshot.getString("Phone"));
+            }
+        });
+
+        resetPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final EditText resetPassword = new EditText(v.getContext());
+                AlertDialog.Builder passwordResetDialong = new AlertDialog.Builder(v.getContext());
+                passwordResetDialong.setTitle("Reset Password");
+                passwordResetDialong.setMessage("Enter New Password > 6 Characters long");
+                passwordResetDialong.setView(resetPassword);
+
+                passwordResetDialong.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //extract the email and send reset link
+                        String newPassword = resetPassword.getText().toString();
+                        user.updatePassword(newPassword).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(actEditProfile.this, "Password Reset Successfully", Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(actEditProfile.this, "Password Reset Failed", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+
+
+                    }
+                });
+
+                passwordResetDialong.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // close
+
+                    }
+                });
+
+                passwordResetDialong.create().show();
+            }
+        });
+
+            }
+        });
     }
 }
