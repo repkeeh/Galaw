9c8d73ddc0c36bba12385ad9dcfade14b4de64f5
diomfz <64569909+diomfz@users.noreply.github.com>
2020-05-07 11:48:10 +0700

    back button
    blm dicoba bisa atau engga

--- app/src/main/java/com/example/galaw/actEditProfile.java
+++ app/src/main/java/com/example/galaw/actEditProfile.java
@@ -10,15 +10,28 @@ import android.widget.Button;
 public class actEditProfile extends AppCompatActivity {
 
     Button save;
-
+    Button backProfile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_act_edit_profile);
 
         save = findViewById(R.id.save);
+        backProfile =findViewById(R.id.backProfile);
+
 
         save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent( actEditProfile.this, fragProfile.class);
+                startActivity(intent);
+
+
+            }
+        });
+
+        backProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
