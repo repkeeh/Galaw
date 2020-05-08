f523ac32ce3e6e8addd17e6dc44562cf4658e887
diomfz <64569909+diomfz@users.noreply.github.com>
2020-05-08 13:01:33 +0700

    actdiary
    savediary button done

--- app/src/main/java/com/example/galaw/actDiary.java
+++ app/src/main/java/com/example/galaw/actDiary.java
@@ -2,13 +2,27 @@ package com.example.galaw;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class actDiary extends AppCompatActivity {
 
+    Button savediary;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_act_diary);
+
+        savediary = findViewById(R.id.savediary);
+
+        savediary.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(actDiary.this, fragProfile.class);
+                startActivity(intent);
+            }
+        });
     }
 }
