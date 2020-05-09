712311b7f166063af6bd189f3d6a183fe42dff8b
repkeeh <64399425+repkeeh@users.noreply.github.com>
2020-05-09 15:08:39 +0700

    fix home profile

--- app/src/main/java/com/example/galaw/actHome.java
+++ app/src/main/java/com/example/galaw/actHome.java
@@ -5,21 +5,50 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
 
 public class actHome extends AppCompatActivity  {
 
 
     Button setting;
+    ImageView profileImage;
+    StorageReference storageReference;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+
     Button[] navigationButton = new Button[4];
     int indexNav;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_act_home);
+
+        profileImage = findViewById(R.id.profileImage);
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+        storageReference = FirebaseStorage.getInstance().getReference();
+
+
+        StorageReference profileRef = storageReference.child("Users/"+fAuth.getCurrentUser().getUid()+"/profile.jpg");
+        profileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Picasso.get().load(uri).into(profileImage);
+            }
+        });
 
         indexNav = 0;
         navigationButton[0] = findViewById(R.id.homeButton);
