889d08916705f38463f8575ef71a40966b6eb07c
diomfz <64569909+diomfz@users.noreply.github.com>
2020-05-10 12:48:00 +0700

    tes

--- app/src/main/java/com/example/galaw/fragPosting.java
+++ app/src/main/java/com/example/galaw/fragPosting.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -23,6 +24,7 @@ public class fragPosting extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
 
     public fragPosting() {
         // Required empty public constructor
