c3cd0765536992c6ab762b042b91c0a293a92a85
diomfz <64569909+diomfz@users.noreply.github.com>
2020-05-13 23:12:06 +0700

    5/13/2020
    gajee

--- app/src/main/java/com/example/galaw/actResult.java
+++ app/src/main/java/com/example/galaw/actResult.java
@@ -2,17 +2,17 @@ package com.example.galaw;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-public class actQuiz extends AppCompatActivity {
+public class actResult extends AppCompatActivity {
 
+
+    Integer result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_act_quiz);
+        setContentView(R.layout.activity_act_result);
+
 
 
     }
