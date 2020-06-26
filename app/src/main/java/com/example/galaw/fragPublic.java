9923c59adfacfe8eb57a6d705a11976afe77af43
diomfz <64569909+diomfz@users.noreply.github.com>
2020-06-26 16:52:05 +0700

    ask
    ask

--- app/src/main/java/com/example/galaw/fragPublic.java
+++ app/src/main/java/com/example/galaw/fragPublic.java
@@ -1,12 +1,16 @@
 package com.example.galaw;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -23,6 +27,7 @@ public class fragPublic extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    Button askButton;
 
     public fragPublic() {
         // Required empty public constructor
@@ -61,4 +66,21 @@ public class fragPublic extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_frag_public, container, false);
     }
+
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        askButton = view.findViewById(R.id.askButton);
+
+        askButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), actDiary.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
 }
